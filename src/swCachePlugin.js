dbca00826aca00dce079d267369d0d100ecc175e
Hosar <hosarsiph.valle@gmail.com>
2018-08-12 19:45:33 -0500

    dev-webpack4 refactor, support for Webpack4 and previous versions

--- src/swCachePlugin.js
+++ src/swCachePlugin.js
@@ -5,12 +5,12 @@ import fs from 'fs';
 import URL from 'url';
 import $debug from 'debug';
 import chalk from 'chalk';
-
+import TemplateCreatorW4 from './TemplateCreatorW4';
 
 Promise.promisifyAll(fs);
 const debug = $debug('app');
-             
-const toClass = {}.toString;           
+
+const toClass = {}.toString;
 
 class SwCachePlugin {
     constructor(options) {
@@ -20,79 +20,79 @@ class SwCachePlugin {
         this.pluginName = 'SwCachePlugin';
     }
 
-    ignoreAssets(assets){
-        this.validateType(this.options.ignore,'Array','Ignore must be an array');
+    ignoreAssets(assets) {
+        this.validateType(this.options.ignore, 'Array', 'Ignore must be an array');
         const ignorePatterns = this.options.ignore || [];
-        
-        if(ignorePatterns)
-            return(assets.filter(text => !ignorePatterns.some(regex => regex.test(text))));
 
-        return assets;        
+        if (ignorePatterns)
+            return (assets.filter(text => !ignorePatterns.some(regex => regex.test(text))));
+
+        return assets;
     }
 
-    validateType(value,type,message){        
-        if(!value)
+    validateType(value, type, message) {
+        if (!value)
             return;
 
         const classOfIgnore = toClass.call(value);
-        if(!classOfIgnore.includes(type))
+        if (!classOfIgnore.includes(type))
             throw new Error(message);
     }
 
-    addAdditionalPaths(additionals, assets){    
-        this.validateType(additionals,'Array','Extras must be an array');
-        if(!additionals)
+    addAdditionalPaths(additionals, assets) {
+        this.validateType(additionals, 'Array', 'Extras must be an array');
+        if (!additionals)
             return assets;
 
         return assets.concat(additionals);
     }
 
-    setPathToAssets(publicPath, assets){
-        const assetGlobs = assets.map(f => URL.resolve(publicPath,f));
+    setPathToAssets(publicPath, assets) {
+        const assetGlobs = assets.map(f => URL.resolve(publicPath, f));
         return assetGlobs;
     }
 
-    getCacheTemplate(){
-        const filePath = path.join(__dirname,'template/sw-cache.tmpl');
-        return fs.readFileAsync(filePath,'utf-8');
+    getCacheTemplate() {
+        const filePath = path.join(__dirname, 'template/sw-cache.tmpl');
+        return fs.readFileAsync(filePath, 'utf-8');
     }
 
-    writeCacheFile(templateWithData){
-        return function(outputPath){
-            const filePath = path.join(outputPath,'/AssetsManager.js');
-            return fs.writeFileAsync(filePath,templateWithData);
+    writeCacheFile(templateWithData) {
+        return function (outputPath) {
+            const filePath = path.join(outputPath, '/AssetsManager.js');
+            return fs.writeFileAsync(filePath, templateWithData);
         }
     }
 
-    populateTemplate(fileTemplate) {        
-        return function(data) {
-            return new Promise((resolve, reject) => {                
+    populateTemplate(fileTemplate) {
+        return function (data) {
+            return new Promise((resolve, reject) => {
                 resolve(template(fileTemplate)(data));
             });
-        }        
+        }
     }
 
-    showCacheEntries(log,cacheEntries){        
+    showCacheEntries(log, cacheEntries) {
         log(chalk.blue('Cache entries:'));
         cacheEntries.map((entry) => {
             log(chalk.green(entry));
         });
-    }   
+    }
 
-    getHashesToSave(assets,hash){
+    getHashesToSave(assets, hash) {
         const noChunks = 2;
         const assetsWithoutChunks = assets.filter(asset => asset.split('.').length === noChunks);
-        const hashes = assetsWithoutChunks.reduce((acc,asset) => {            
-            if(!asset.includes(hash)){
+        const hashes = assetsWithoutChunks.reduce((acc, asset) => {
+            if (!asset.includes(hash)) {
                 const parts = asset.split('.');
                 acc.push(parts[0]);
             }
             return acc;
-        },[hash]);
+        }, [hash]);
         return hashes;
     }
 
-    arrayToString(arr){
+    arrayToString(arr) {
         const withFormat = arr.map((a) => {
             return '\'' + a + '\'';
         }).join(',');
@@ -100,57 +100,65 @@ class SwCachePlugin {
         return withFormat;
     }
 
-    addOriginPath(_url = '',publicPath = ''){
+    addOriginPath(_url = '', publicPath = '') {
         const parsedUrl = URL.parse(_url);
-        if(parsedUrl.host)
+        if (parsedUrl.host)
             return parsedUrl.href;
 
         const _originPath = URL.parse(publicPath);
         const mainUrl = `${_originPath.protocol}//${_originPath.host}`;
-        const completeUrl = URL.resolve(mainUrl,_url);
+        const completeUrl = URL.resolve(mainUrl, _url);
         return completeUrl;
     }
 
-    formatToShow(urls = [], publicPath = ''){
+    formatToShow(urls = [], publicPath = '') {
         const _urls = urls.map(url => {
-            return this.addOriginPath(url,publicPath);
+            return this.addOriginPath(url, publicPath);
         });
 
         return _urls;
     }
 
-    apply(compiler) {
+    getTemplateCreator(compiler) {
+        if(!compiler.hooks){
+            return new TemplateCreatorW2(compiler);
+        }
+        return new TemplateCreatorW4(compiler);
+    }
 
-        compiler.hooks.afterEmit.tapAsync({
-            name: this.pluginName
-        } ,(compilation, callback) => {
-            const hash = compilation.hash;
+    apply(compiler) {
+        let templateCreator = this.getTemplateCreator(compiler);
+        const fn = (compiler, compilation, callback) => {
             const outputPath = compiler.options.output.path;
+            const hash = compilation.hash;
             const publicPath = compiler.options.output.publicPath;
             const additionals = this.options.include;
             const cacheName = this.options.cacheName;
-            const assets = Object.keys(compilation.assets) || [];
+            const _assets = templateCreator.getAssets();
+            const assets = Object.keys(_assets) || [];
             const filteredAssets = this.ignoreAssets(assets);
             const hashesToSave = this.getHashesToSave(filteredAssets, hash);
-            const cacheEntries = this.setPathToAssets(publicPath,filteredAssets);
-            const cacheEntries_ = this.addAdditionalPaths(additionals,cacheEntries);
-            
+            const cacheEntries = this.setPathToAssets(publicPath, filteredAssets);
+            const cacheEntries_ = this.addAdditionalPaths(additionals, cacheEntries);
+
             const templateInfo = {
                 cacheEntries: this.arrayToString(cacheEntries_),
                 cacheName: cacheName,
                 hashes: this.arrayToString(hashesToSave)
             }
 
-            const urlsToShow = this.formatToShow(cacheEntries_,publicPath);
+            const urlsToShow = this.formatToShow(cacheEntries_, publicPath);
 
             this.getCacheTemplate()
                 .then(fileTemplate => this.populateTemplate(fileTemplate)(templateInfo))
                 .then(templateWithData => this.writeCacheFile(templateWithData)(outputPath))
-                .then(success => {this.showCacheEntries(console.log, urlsToShow)},
-                      err => {throw new Error(err)});
-            
+                .then(success => { this.showCacheEntries(console.log, urlsToShow) },
+                    err => { throw new Error(err) });
+
             callback();
-        });
+        };
+
+        templateCreator.createTemplate(fn);
     }
 }
 
